61bd1b0432d90cf9b1fc3a2a2639aaadfda5bfa8
TMaglietta <36413885+TMaglietta@users.noreply.github.com>
2018-10-31 22:56:16 -0400

    added lower_q_tester_1 and listed backlog

--- statTools.py
+++ statTools.py
@@ -1,1 +1,7 @@
-
+"""
+work on:
+lower_quartile
+upper_quartile
+variance
+standard deviation
+"""
--- test_statTools.py
+++ test_statTools.py
@@ -1,3 +1,9 @@
 import pytest
 from statTools import *
 
+
+def lower_q_tester_1():
+    assert(lower_quartile() == 0)
+
+
+lower_q_tester_1()
