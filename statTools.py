9a2ceee9472ae93e2a2f2514df72ed7b2dc84007
TMaglietta <36413885+TMaglietta@users.noreply.github.com>
2018-11-06 16:34:41 -0500

    refactoring LQ

--- statTools.py
+++ statTools.py
@@ -7,23 +7,21 @@ mode
 """
 
 
-def lower_quartile(num_list: list)->float:
+def lower_quartile(num_list: list) -> float:
     """
-    median of lower half of list
-    include median when searching in an odd sized list
-    use nums before median when using an even sized list
+    -median of lower half of list
+    -include median when searching in an odd sized list
+    -use nums before median when using an even sized list
     """
-    num_list.sort()
+    num_list.sort()  # sort the list or it will not be accurate
     key_index = len(num_list) // 4
-    if len(num_list) % 4 == 2: #check for even sized lists where the LQ is in the list
+    if len(num_list) % 4 == 2 or len(num_list) % 4 == 1:  # check for lists where the LQ is in the list
         return num_list[key_index]
-    elif len(num_list) % 4 == 0: #check if list is even and LQ is found an average of 2 numbers
+    elif len(num_list) % 4 == 0:  # check if list is even
         num_1 = num_list[key_index]
-        num_2 = num_list[key_index - 1]
-        return (num_1 + num_2)/2
-    elif len(num_list) % 4 == 3:
+        num_2 = num_list[key_index - 1]  # LQ found is an average of 2 numbers
+        return (num_1 + num_2) / 2
+    elif len(num_list) % 4 == 3:  # check if list is even
         num_1 = num_list[key_index]
         num_2 = num_list[key_index + 1]
-        return (num_1 + num_2) / 2
-    else:
-        return num_list[key_index]
+        return (num_1 + num_2) / 2  # LQ found is an average of 2 numbers
--- test_statTools.py
+++ test_statTools.py
@@ -1,24 +1,27 @@
 import pytest
 from statTools import *
+# lower quartile tests
 
 
 def lower_q_test_1():
-    assert(lower_quartile([0, 50, 100, 150]) == 25)
+    assert(lower_quartile([0, 50, 100, 150]) == 25)  # corner case--LQ is the average of 2 numbers
 
 
 def lower_q_test_2():
-    assert(lower_quartile([0, 25, 50, 75, 100, 125]) == 25)
+    assert(lower_quartile([0, 25, 50, 75, 100, 125]) == 25)  # general case--returns item from index in list
 
 
 def lower_q_test_3():
-    assert (lower_quartile([0, 25, 50, 75, 100, 125, 150]) == 37.5)
+    assert (lower_quartile([0, 25, 50, 75, 100, 125, 150]) == 37.5)  # corner case--LQ is the average of 2 numbers
 
 
 def lower_q_test_4():
-    assert (lower_quartile([1, 2, 3, 4, 5, 6, 7, 8, 9]) == 3)
+    assert (lower_quartile([1, 2, 3, 4, 5, 6, 7, 8, 9]) == 3)  # general case--returns item from index in list
+
+# upper quartile tests
 
 
 lower_q_test_1()
 lower_q_test_2()
-#lower_q_test_3()
+lower_q_test_3()
 lower_q_test_4()
