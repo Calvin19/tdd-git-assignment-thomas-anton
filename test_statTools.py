c2a9c47a14553d8d594ba5ab73f0d94d4a2d5644
TMaglietta <36413885+TMaglietta@users.noreply.github.com>
2018-11-06 23:06:37 -0500

    made standardized test lists for future testing convinience

--- test_statTools.py
+++ test_statTools.py
@@ -12,23 +12,28 @@ Created: 31/10/2018
 import pytest
 from statTools import *
 
+
+test_list_1 = [1, 2, 3, 4]
+test_list_2 = [1, 2, 3, 4, 5, 6]
+test_list_3 = [1, 2, 3, 4, 5, 6, 7]
+test_list_4 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # lower quartile tests
 
 
 def lower_q_test_1():
-    assert(lower_quartile([0, 50, 100, 150]) == 25)  # corner case--LQ is the average of 2 numbers
+    assert(lower_quartile(test_list_1) == 1.5)  # corner case--LQ is the average of 2 numbers
 
 
 def lower_q_test_2():
-    assert(lower_quartile([0, 25, 50, 75, 100, 125]) == 25)  # general case--returns item from index in list
+    assert(lower_quartile(test_list_2) == 2)  # general case--returns item from index in list
 
 
 def lower_q_test_3():
-    assert (lower_quartile([0, 25, 50, 75, 100, 125, 150]) == 37.5)  # corner case--LQ is the average of 2 numbers
+    assert (lower_quartile(test_list_3) == 2.5)  # corner case--LQ is the average of 2 numbers
 
 
 def lower_q_test_4():
-    assert (lower_quartile([1, 2, 3, 4, 5, 6, 7, 8, 9]) == 3)  # general case--returns item from index in list
+    assert (lower_quartile(test_list_4) == 3)  # general case--returns item from index in list
 
 # upper quartile tests
 
@@ -46,5 +51,5 @@ lower_q_test_2()
 lower_q_test_3()
 lower_q_test_4()
 
-upper_q_test_1()
-upper_q_test_2()
+#upper_q_test_1()
+#upper_q_test_2()
