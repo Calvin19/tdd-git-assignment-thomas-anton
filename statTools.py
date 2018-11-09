a59bf6c1d5b6e20c9403034e3bfdbfb1160d42f8
TMaglietta <36413885+TMaglietta@users.noreply.github.com>
2018-11-08 22:14:35 -0500

    UQ complete, will return for exceptions

--- statTools.py
+++ statTools.py
@@ -27,7 +27,7 @@ def lower_quartile(num_list: list):
             num_1 = num_list[key_index]
             num_2 = num_list[key_index - 1]  # LQ found is an average of 2 numbers
             return (num_1 + num_2) / 2
-        elif len(num_list) % 4 == 3:  # check if list is even
+        elif len(num_list) % 4 == 3:  # check if list is odd
             num_1 = num_list[key_index]
             num_2 = num_list[key_index + 1]
             return (num_1 + num_2) / 2  # LQ found is an average of 2 numbers
@@ -40,17 +40,21 @@ def lower_quartile(num_list: list):
 
 
 def upper_quartile(num_list):
+    """
+    Finds the upper quartile in a list of integers
+
+    :param num_list: list of integers to find the upper quartile of
+    :return: the upper quartile of num_list
+    """
     num_list.sort()
     key_index = (len(num_list)-1) - (len(num_list)//4)
-    if len(num_list) % 4 == 2:
+    if len(num_list) % 4 == 2 or len(num_list) % 4 == 1:  # check for lists where UQ is in list
         return num_list[key_index]
-    elif len(num_list) % 4 == 0:
+    elif len(num_list) % 4 == 0:  # check for lists where UQ is the average between to numbers
         num_1 = num_list[key_index]
         num_2 = num_list[key_index + 1]
         return (num_1 + num_2) / 2
-    elif len(num_list) % 4 == 3:
+    elif len(num_list) % 4 == 3:  # check for lists where UQ is the average between to numbers
         num_1 = num_list[key_index]
         num_2 = num_list[key_index - 1]
         return (num_1 + num_2) / 2
-    elif len(num_list) % 4 == 1:
-        return num_list[key_index]
