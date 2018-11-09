7cd686411970a3bb9847a2f1fd90577c1f8765ef
TMaglietta <36413885+TMaglietta@users.noreply.github.com>
2018-11-08 20:33:05 -0500

    moving on to UQ will return to LQ to fix exception issues

--- statTools.py
+++ statTools.py
@@ -1,12 +1,12 @@
 """
-----------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
 Filename: statTools.py
 Purpose: provide tools in order to find statistics: median, mode, lower quartile, and upper quartile
 
 Author: Maglietta.T
 
 Created: 31/10/2018
-----------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
 """
 
 
@@ -17,18 +17,24 @@ def lower_quartile(num_list: list) -> float:
     :param num_list: list of integers to find the lower quartile of
     :return: the lower quartile of num_list
     """
-    num_list.sort()  # sort the list or it will not be accurate
-    key_index = len(num_list) // 4
-    if len(num_list) % 4 == 2 or len(num_list) % 4 == 1:  # check for lists where the LQ is in the list
-        return num_list[key_index]
-    elif len(num_list) % 4 == 0:  # check if list is even
-        num_1 = num_list[key_index]
-        num_2 = num_list[key_index - 1]  # LQ found is an average of 2 numbers
-        return (num_1 + num_2) / 2
-    elif len(num_list) % 4 == 3:  # check if list is even
-        num_1 = num_list[key_index]
-        num_2 = num_list[key_index + 1]
-        return (num_1 + num_2) / 2  # LQ found is an average of 2 numbers
+
+    try:
+        num_list.sort()  # sort the list or it will not be accurate
+        key_index = len(num_list) // 4
+        if len(num_list) % 4 == 2 or len(num_list) % 4 == 1:  # check for lists where the LQ is in the list
+            return num_list[key_index]
+        elif len(num_list) % 4 == 0:  # check if list is even
+            num_1 = num_list[key_index]
+            num_2 = num_list[key_index - 1]  # LQ found is an average of 2 numbers
+            return (num_1 + num_2) / 2
+        elif len(num_list) % 4 == 3:  # check if list is even
+            num_1 = num_list[key_index]
+            num_2 = num_list[key_index + 1]
+            return (num_1 + num_2) / 2  # LQ found is an average of 2 numbers
+    except ValueError:
+        print('Name Error')
+    except IndexError:
+        print('Index Error')
 
 
 def upper_quartile(num_list) -> float:
--- test_statTools.py
+++ test_statTools.py
@@ -17,6 +17,12 @@ test_list_1 = [1, 2, 3, 4]
 test_list_2 = [1, 2, 3, 4, 5, 6]
 test_list_3 = [1, 2, 3, 4, 5, 6, 7]
 test_list_4 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+test_list_5 = [1, 2, 3]
+test_list_6 = [1, 2]
+test_list_7 = [1]
+test_list_8 = []
+test_not_list = 'a'
+test_str_list = ['a', 'b', 'c', 'd', 'e', 'f']
 # lower quartile tests
 
 
@@ -35,21 +41,52 @@ def lower_q_test_3():
 def lower_q_test_4():
     assert (lower_quartile(test_list_4) == 3)  # general case--returns item from index in list
 
+
+def lower_q_test_5():
+    assert (lower_quartile(test_list_5) == 1.5)
+
+
+def lower_q_test_6():
+    assert (lower_quartile(test_list_6) == 1)
+
+
+def lower_q_test_7():
+    assert (lower_quartile(test_list_7) == 1)
+
+
+def lower_q_test_8():
+    assert (lower_quartile(test_list_8) is None)
+
+
+def lower_q_test_9():
+    assert (lower_quartile(test_not_list) is TypeError)
+
+
+def lower_q_test_10():
+    assert (lower_quartile(test_str_list) == '4')
+
 # upper quartile tests
 
-
+"""
 def upper_q_test_1():
     assert (upper_quartile() == 5)
 
 
 def upper_q_test_2():
     assert (upper_quartile() == 3)
-
+"""
 
 lower_q_test_1()
 lower_q_test_2()
 lower_q_test_3()
 lower_q_test_4()
+lower_q_test_5()
+lower_q_test_6()
+lower_q_test_7()
+#lower_q_test_8()
+#lower_q_test_9()
+lower_q_test_10
+
 
 #upper_q_test_1()
 #upper_q_test_2()
