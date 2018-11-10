a99f2786fafe11ef52fbf553fdd9c68ed34eb697
AntonSchoolSTRobs <44612722+AntonSchoolSTRobs@users.noreply.github.com>
2018-11-10 14:49:07 -0500

    Created first two functions + placeholders

--- statTools.py
+++ statTools.py
@@ -1,1 +1,27 @@
+def mean(alist):
+    try:
+        listclone = alist
+        numsum = 0
+        numnum = 0
+        for item in listclone:
+            numsum = numsum + item
+            numnum += 1
+        finalnum = numsum / numnum
+        return finalnum
+    except:
+        return "Invalid Input, please try again!"
 
+
+def numrange(alist):
+    try:
+        fRange = max(alist) - min(alist)
+        return fRange
+    except:
+        return "Invalid Input, please try again!"
+
+
+def standev(alist):
+    return
+
+def variance(alist):
+    return
